11d473c52efcb89977762595a3bdc32c76de5b9e
MajorTom <ivansantander2020@gmail.com>
2022-02-03 19:39:34 -0500

    REST API LISTA

--- src/pages/api/task/[id].ts
+++ src/pages/api/task/[id].ts
@@ -1,15 +1,45 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { connect } from 'src/utils/database'
+
 // eslint-disable-next-line import/no-anonymous-default-export
-export default (req:NextApiRequest,res:NextApiResponse)=>{
-    const { method } = req
+export default async (req:NextApiRequest,res:NextApiResponse)=>{
+    const { method,query,body } = req
 
     switch (method) {
         case 'GET':
-            return res.status(200).json('get task')
+            try {
+                const text = "SELECT * FROM tasks WHERE id = $1"
+                const values = [query.id]
+                const response = await connect.query(text,values)
+                if (response.rows.length === 0)
+                    res.status(400).json({message:"Task not found"})
+                return res.status(200).json(response.rows[0])    
+            } catch (error:any) {
+                res.status(400).json({error:error.message})
+            }
         case 'PUT':
-            return res.status(200).json('update task')
+            try {
+                const { title,description } = body
+                const text = "UPDATE tasks SET title=$1, description=$2 WHERE id = $3 RETURNING *"
+                const values = [title,description,query.id]
+                const response = await connect.query(text,values)
+                if (response.rowCount === 0)
+                    res.status(400).json({message:"Task not found"})
+                return res.status(200).json(response.rows[0])    
+            } catch (error:any) {
+                res.status(400).json({error:error.message})
+            }
         case 'DELETE':
-            return res.status(200).json('delete task')
+            try {
+                const text = "DELETE FROM tasks WHERE id = $1 RETURNING *"
+                const values = [query.id]
+                const response = await connect.query(text,values)
+                if (response.rowCount === 0)
+                    res.status(400).json({message:"Task not found"})
+                return res.status(200).json(response.rows[0])    
+            } catch (error:any) {
+                res.status(400).json({error:error.message})
+            }
         default:
             return res.status(400).json('method not allowed')
     }
--- src/pages/api/task/index.ts
+++ src/pages/api/task/index.ts
@@ -1,14 +1,29 @@
 import { NextApiRequest, NextApiResponse } from "next"
-
+import { connect } from "src/utils/database"
 // eslint-disable-next-line import/no-anonymous-default-export
-export default (req:NextApiRequest,res:NextApiResponse)=>{
-    const { method } = req
+export default async (req:NextApiRequest,res:NextApiResponse)=>{
+    const { method,body } = req
 
     switch (method) {
         case 'GET':
-            return res.status(200).json('get tasks')
+            try {
+                const query = 'SELECT * FROM tasks'
+                const response = await connect.query(query)
+                console.log(response.rows[0])
+                return res.status(200).json(response.rows)
+            } catch (error:any) {
+                res.status(400).json({ error:error.message })
+            }
         case 'POST':
-            return res.status(200).json('creating tasks')
+            try {
+                const {title, description} = body;
+                const query = 'INSERT INTO public.tasks(title, description) VALUES ($1,$2) RETURNING *'
+                const values = [title,description]
+                const response = await connect.query(query,values)
+                return res.status(200).json(response.rows[0])
+            } catch (error) {
+                console.error(error)
+            }
         case 'PUT':
             return res.status(200).json('update tasks')
         case 'DELETE':
