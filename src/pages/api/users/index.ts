471c9a4cb9ac1df580850e0a9eb4da53b3702a50
MajorTom <ivansantander2020@gmail.com>
2022-02-03 22:17:49 -0500

    REST API USERS OK

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -8,7 +8,7 @@ export default async(req:NextApiRequest,res:NextApiResponse)=>{
         case 'GET':
             try {
                 const query = 'SELECT * FROM users'
-                const response = connect.query(query)
+                const response = await connect.query(query)
                 return res.status(200).json(response.rows)
             } catch (error:any) {
                 res.status(400).json({error:error.message})
