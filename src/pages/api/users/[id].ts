d214c118eacd0172eea8e345ed6115073fec484b
MajorTom <ivansantander2020@gmail.com>
2022-02-05 21:49:31 -0500

    consulta por backend a la api e integracion de la interface

--- src/pages/api/users/[id].ts
+++ src/pages/api/users/[id].ts
@@ -1,48 +1,43 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import { connect } from 'src/utils/database'
+import {NextApiRequest, NextApiResponse} from 'next'
+import {connect} from 'src/utils/database'
 
-// eslint-disable-next-line import/no-anonymous-default-export
 export default async (req:NextApiRequest,res:NextApiResponse)=>{
-    const {method,body,query} = req
-    
+    const { body,query,method } = req;
     switch (method) {
-        case "GET":
+        case 'GET':
             try {
-                const sql = 'SELECT * FROM users WHERE id = $1'
+                const sql = "SELECT * FROM users WHERE id=$1"
                 const values = [query.id]
-                const response = await connect.query(sql,values)
-                if(response.rowCount === 0)
-                    return res.status(400).json({message:"User not found"})
+                const response = await connect.query(sql,values);
+                if (response.rowCount===0)
+                    return res.status(400).json('user not found')
                 return res.status(200).json(response.rows)
             } catch (error:any) {
-                res.status(400).json({error:error.message})
+                return res.status(400).json({error:error.message})
             }
-
-        case "PUT":
+        case 'PUT':
             try {
-                const { name,user,estado } = body
+                const {name,user,estado} = body;
                 const sql = 'UPDATE users SET name=$1, "user"=$2, estado=$3 WHERE id = $4 RETURNING *'
                 const values = [name,user,estado,query.id]
                 const response = await connect.query(sql,values)
                 if(response.rowCount === 0)
-                    return res.status(400).json({message:"User not found"})
+                    res.status(400).json('User not found')
+                res.status(200).json(response.rows)
+            } catch (error:any) {
+                return res.status(400).json({error:error.message})
+            }
+        case 'DELETE':
+            try {
+                const sql = 'DELETE FROM users WHERE id = $1 RETURNING *'
+                const values = [query.id]
+                const response = connect.query(sql,values)
+                if(response.rows===0)
+                    return res.status(400).json('User not found')
                 return res.status(200).json(response.rows)
             } catch (error:any) {
-                res.status(400).json({error:error.message})
+                return res.status(400).json({error:error.message})
             }
-        
-        case "DELETE":
-        try {
-            const sql = 'DELETE FROM users WHERE id = $1 RETURNING *'
-            const values = [query.id]
-            const response = await connect.query(sql,values)
-            if(response.rowCount === 0)
-                return res.status(400).json({message:"User not found"})
-            return res.status(200).json(response.rows)
-        } catch (error:any) {
-            res.status(400).json({error:error.message})
-        }
-    
         default:
             break;
     }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,21 @@
-export default function index(){
-  return(
-    <div>
-      Hi humans
-    </div>
-  )
+import {Task} from 'src/interfaces/Task'
+
+interface Props{
+  tasks:Task[]
+}
+
+export default function index({tasks}:Props){
+  return <>{tasks.length === 0 ? <h1>no task</h1> : <h1>tasks</h1>}</>
+}
+
+export const getServerSideProps = async () => {
+
+    const res = await fetch('http://localhost:3000/api/task');
+    const tasks = await res.json()
+
+    return {
+      props:{
+        tasks:tasks
+      }
+    }
 }
